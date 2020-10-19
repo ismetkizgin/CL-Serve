22a364e6495bb03791e16b74a34203fd296c5fae
ismet kizgin <info@ismetkizgin.com>
2020-10-19 21:48:13 +0300

    refactor: validators revised

--- middleware/validators/userValidator.js
+++ middleware/validators/userValidator.js
@@ -4,11 +4,6 @@ const HttpStatusCode = require('http-status-codes');
 class UserValidator {
     constructor() { }
 
-    errorResponse = {
-        status: HttpStatusCode.EXPECTATION_FAILED,
-        message: 'Must have correct data entry.'
-    }
-
     static async list(req, res, next) {
         try {
             await joi.object({
@@ -17,7 +12,7 @@ class UserValidator {
             }).with('offset', 'limit').validateAsync(req.body);
             next();
         } catch (error) {
-            res.status(errorResponse.status).json(errorResponse.message);
+            res.status(HttpStatusCode.EXPECTATION_FAILED).send('Must have correct data entry.');
         }
     }
 }
