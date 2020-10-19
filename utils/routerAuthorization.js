d4f1c776a680e148b6d830d0bef8407b5c7fb09e
ismet kizgin <info@ismetkizgin.com>
2020-10-19 22:32:16 +0300

    feat: added user delete router

--- utils/routerAuthorization.js
+++ utils/routerAuthorization.js
@@ -4,6 +4,9 @@ module.exports = {
     user: {
         GET: {
             Authorize: [Roles.Root, Roles.Administrator]
-        }
+        },
+        DELETE: {
+            Authorize: [Roles.Root, Roles.Administrator]
+        },
     }
 }
