ad9a00a6dc6308707179c37c208b0dfc2598e5f3
ismet kizgin <info@ismetkizgin.com>
2020-10-20 04:25:23 +0300

    feat: added database test data script

---
+++ dbBackup/test_data.sql
@@ -0,0 +1,9 @@
+--
+--  tblUser
+--
+
+INSERT INTO `tblUser` (`UserID`, `UserFirstName`, `UserLastName`, `UserEmail`, `UserPassword`, `UserDateOfBirth`, `UserTypeName`) VALUES ('1', 'İsmet', 'Kizgin', 'cl@project.com', 'password', '2020-10-15', 'Root'); 
+INSERT INTO `tblUser` (`UserID`, `UserFirstName`, `UserLastName`, `UserEmail`, `UserPassword`, `UserDateOfBirth`, `UserTypeName`) VALUES ('2', 'İsmet', 'Kizgin', 'cl1@project.com', 'password', '2020-10-15', 'Administrator'); 
+INSERT INTO `tblUser` (`UserID`, `UserFirstName`, `UserLastName`, `UserEmail`, `UserPassword`, `UserDateOfBirth`, `UserTypeName`) VALUES ('3', 'İsmet', 'Kizgin', 'cl3@project.com', 'password', '2020-10-15', 'Developer'); 
+INSERT INTO `tblUser` (`UserID`, `UserFirstName`, `UserLastName`, `UserEmail`, `UserPassword`, `UserDateOfBirth`, `UserTypeName`) VALUES ('4', 'İsmet', 'Kizgin', 'cl2@project.com', 'password', '2020-10-15', 'Editor'); 
+INSERT INTO `tblUser` (`UserID`, `UserFirstName`, `UserLastName`, `UserEmail`, `UserPassword`, `UserDateOfBirth`, `UserTypeName`) VALUES ('5', 'İsmet', 'Kizgin', 'cl4@project.com', 'password', '2020-10-15', 'User'); 
