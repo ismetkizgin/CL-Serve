db715e68faaf28a967bf4137168f91947b6d0df3
ismet kizgin <info@ismetkizgin.com>
2021-01-01 01:23:47 +0300

    feat: added sign up router

--- middleware/validators/authValidator.js
+++ middleware/validators/authValidator.js
@@ -1,69 +1,123 @@
-const joi = require('joi');
-const HttpStatusCode = require('http-status-codes');
-
+const joi = require("joi");
+const HttpStatusCode = require("http-status-codes");
 
 class AuthValidator {
-    constructor() { }
+  constructor() {}
 
-    static async login(req, res, next) {
-        try {
-            await joi.object({
-                UserEmail: joi.string().email().required(),
-                UserPassword: joi.string().required()
-            }).validateAsync(req.body);
-            next();
-        } catch (error) {
-            res.status(HttpStatusCode.EXPECTATION_FAILED).send('Must have correct data entry.');
-        }
+  static async login(req, res, next) {
+    try {
+      await joi
+        .object({
+          UserEmail: joi.string().email().required(),
+          UserPassword: joi.string().required(),
+        })
+        .validateAsync(req.body);
+      next();
+    } catch (error) {
+      res
+        .status(HttpStatusCode.EXPECTATION_FAILED)
+        .send("Must have correct data entry.");
     }
+  }
 
-    static async update(req, res, next) {
-        try {
-            await joi.object({
-                UserFirstName: joi.string().min(3).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı ]+$')).required(),
-                UserLastName: joi.string().min(3).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı ]+$')).required(),
-                UserDateOfBirth: joi.date().required(),
-                UserPassword: joi.string().max(99).required(),
-            }).validateAsync(req.body);
-            next();
-        } catch (error) {
-            res.status(HttpStatusCode.EXPECTATION_FAILED).send('Must have correct data entry.');
-        }
+  static async signUp(req, res, next) {
+    try {
+      await joi
+        .object({
+          UserFirstName: joi
+            .string()
+            .min(3)
+            .pattern(new RegExp("^[A-Za-zÇçÖöŞşÜüĞğİı ]+$"))
+            .required(),
+          UserLastName: joi
+            .string()
+            .min(3)
+            .pattern(new RegExp("^[A-Za-zÇçÖöŞşÜüĞğİı ]+$"))
+            .required(),
+          UserEmail: joi.string().email().required(),
+          UserDateOfBirth: joi.date().required(),
+          UserPassword: joi.string().required(),
+        })
+        .validateAsync(req.body);
+      next();
+    } catch (error) {
+      res
+        .status(HttpStatusCode.EXPECTATION_FAILED)
+        .send("Must have correct data entry.");
     }
+  }
 
-    static async delete(req, res, next) {
-        try {
-            await joi.object({
-                UserPassword: joi.string().max(99).required(),
-            }).validateAsync(req.body);
-            next();
-        } catch (error) {
-            res.status(HttpStatusCode.EXPECTATION_FAILED).send('Must have correct data entry.');
-        }
+  static async update(req, res, next) {
+    try {
+      await joi
+        .object({
+          UserFirstName: joi
+            .string()
+            .min(3)
+            .pattern(new RegExp("^[A-Za-zÇçÖöŞşÜüĞğİı ]+$"))
+            .required(),
+          UserLastName: joi
+            .string()
+            .min(3)
+            .pattern(new RegExp("^[A-Za-zÇçÖöŞşÜüĞğİı ]+$"))
+            .required(),
+          UserDateOfBirth: joi.date().required(),
+          UserPassword: joi.string().max(99).required(),
+        })
+        .validateAsync(req.body);
+      next();
+    } catch (error) {
+      res
+        .status(HttpStatusCode.EXPECTATION_FAILED)
+        .send("Must have correct data entry.");
     }
+  }
 
-    static async changePassword(req, res, next) {
-        try {
-            await joi.object({
-                UserPassword: joi.string().max(99).required(),
-                UserNewPassword: joi.string().max(99).required()
-            }).validateAsync(req.body);
-            next();
-        } catch (error) {
-            res.status(HttpStatusCode.EXPECTATION_FAILED).send('Must have correct data entry.');
-        }
+  static async delete(req, res, next) {
+    try {
+      await joi
+        .object({
+          UserPassword: joi.string().max(99).required(),
+        })
+        .validateAsync(req.body);
+      next();
+    } catch (error) {
+      res
+        .status(HttpStatusCode.EXPECTATION_FAILED)
+        .send("Must have correct data entry.");
     }
+  }
 
-    static async passwordControl(req, res, next) {
-        try {
-            await joi.object({
-                UserPassword: joi.string().max(99).required(),
-            }).validateAsync(req.body);
-            next();
-        } catch (error) {
-            res.status(HttpStatusCode.EXPECTATION_FAILED).send('Must have correct data entry.');
-        }
+  static async changePassword(req, res, next) {
+    try {
+      await joi
+        .object({
+          UserPassword: joi.string().max(99).required(),
+          UserNewPassword: joi.string().max(99).required(),
+        })
+        .validateAsync(req.body);
+      next();
+    } catch (error) {
+      res
+        .status(HttpStatusCode.EXPECTATION_FAILED)
+        .send("Must have correct data entry.");
     }
+  }
+
+  static async passwordControl(req, res, next) {
+    try {
+      await joi
+        .object({
+          UserPassword: joi.string().max(99).required(),
+        })
+        .validateAsync(req.body);
+      next();
+    } catch (error) {
+      res
+        .status(HttpStatusCode.EXPECTATION_FAILED)
+        .send("Must have correct data entry.");
+    }
+  }
 }
 
 module.exports = AuthValidator;
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,65 +1,128 @@
-const router = require('express')();
-const jwt = require('jsonwebtoken');
-const TransactionsFactory = require('../database/transactionFactory');
-const { validators, verifyToken } = require('../middleware');
-const userTransactions = TransactionsFactory.creating('userTransactions');
+const router = require("express")();
+const jwt = require("jsonwebtoken");
+const TransactionsFactory = require("../database/transactionFactory");
+const { validators, verifyToken } = require("../middleware");
+const userTransactions = TransactionsFactory.creating("userTransactions");
 const authValidator = validators.authValidator;
 const tokenControl = verifyToken.tokenControl;
-const HttpStatusCode = require('http-status-codes');
-const moment = require('moment');
+const HttpStatusCode = require("http-status-codes");
+const moment = require("moment");
+const Roles = require("../models/roles");
 
-router.post('/login', authValidator.login, async (req, res) => {
-    try {
-        const result = await userTransactions.loginAsync(req.body);
-        const payload = { UserID: result.UserID, UserTypeName: result.UserTypeName }
-        const token = jwt.sign(payload, req.app.get('api_key'), { expiresIn: '7d' });
-        res.json({ result, token });
-    } catch (error) {
-        res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
-    }
+router.post("/login", authValidator.login, async (req, res) => {
+  try {
+    const result = await userTransactions.loginAsync(req.body);
+    const payload = {
+      UserID: result.UserID,
+      UserTypeName: result.UserTypeName,
+    };
+    const token = jwt.sign(payload, req.app.get("api_key"), {
+      expiresIn: "7d",
+    });
+    res.json({ result, token });
+  } catch (error) {
+    res
+      .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+      .send(error.message);
+  }
 });
 
-router.delete('/my-account', tokenControl, authValidator.delete, async (req, res) => {
-    try {
-        const result = await userTransactions.deleteAsync(req.decode.UserID);
-        res.json(result);
-    } catch (error) {
-        res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
-    }
+router.post("/sign-up", authValidator.signUp, async (req, res) => {
+  try {
+    req.body.UserDateOfBirth = moment(
+      new Date(req.body.UserDateOfBirth)
+    ).format("YYYY/MM/DD");
+    req.body.UserTypeName = Roles.User;
+    const result = await userTransactions.insertAsync(req.body);
+    res.json(result);
+  } catch (error) {
+    res
+      .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+      .send(error.message);
+  }
 });
 
-router.put('/my-account', tokenControl, authValidator.update, async (req, res) => {
+router.delete(
+  "/my-account",
+  tokenControl,
+  authValidator.delete,
+  async (req, res) => {
     try {
-        await userTransactions.passwordControlAsync({ UserID: req.decode.UserID, UserPassword: req.body.UserPassword });
-        req.body.UserDateOfBirth = moment(new Date(req.body.UserDateOfBirth)).format('YYYY/MM/DD');
-        const result = await userTransactions.updateAsync(Object.assign(req.body, { UserID: req.decode.UserID }));
-        res.json(result);
+      const result = await userTransactions.deleteAsync(req.decode.UserID);
+      res.json(result);
     } catch (error) {
-        res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
+      res
+        .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(error.message);
     }
-});
+  }
+);
 
-router.put('/change-password', tokenControl, authValidator.changePassword, async (req, res) => {
+router.put(
+  "/my-account",
+  tokenControl,
+  authValidator.update,
+  async (req, res) => {
     try {
-        const result = await userTransactions.changePasswordAsync(Object.assign(req.body, { UserID: req.decode.UserID }));
-        res.json(result);
+      await userTransactions.passwordControlAsync({
+        UserID: req.decode.UserID,
+        UserPassword: req.body.UserPassword,
+      });
+      req.body.UserDateOfBirth = moment(
+        new Date(req.body.UserDateOfBirth)
+      ).format("YYYY/MM/DD");
+      const result = await userTransactions.updateAsync(
+        Object.assign(req.body, { UserID: req.decode.UserID })
+      );
+      res.json(result);
     } catch (error) {
-        res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
+      res
+        .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(error.message);
     }
-});
+  }
+);
 
-router.put('/password-control', tokenControl, authValidator.passwordControl, async (req, res) => {
+router.put(
+  "/change-password",
+  tokenControl,
+  authValidator.changePassword,
+  async (req, res) => {
     try {
-        const result = await userTransactions.passwordControlAsync({ UserID: req.decode.UserID, UserPassword: req.body.UserPassword });
-        res.json(result);
+      const result = await userTransactions.changePasswordAsync(
+        Object.assign(req.body, { UserID: req.decode.UserID })
+      );
+      res.json(result);
     } catch (error) {
-        console.log(error);
-        res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
+      res
+        .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(error.message);
     }
-});
+  }
+);
 
-router.get('/token-decode', tokenControl, async (req, res) => {
-    res.json(req.decode);
+router.put(
+  "/password-control",
+  tokenControl,
+  authValidator.passwordControl,
+  async (req, res) => {
+    try {
+      const result = await userTransactions.passwordControlAsync({
+        UserID: req.decode.UserID,
+        UserPassword: req.body.UserPassword,
+      });
+      res.json(result);
+    } catch (error) {
+      console.log(error);
+      res
+        .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(error.message);
+    }
+  }
+);
+
+router.get("/token-decode", tokenControl, async (req, res) => {
+  res.json(req.decode);
 });
 
 module.exports = router;
