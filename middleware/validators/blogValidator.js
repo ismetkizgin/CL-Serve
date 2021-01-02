9c1558846a8dc904ca6491be7985b86828876ff7
ismet kizgin <info@ismetkizgin.com>
2021-01-02 18:09:12 +0300

    refactor: blog validator revised

--- middleware/validators/blogValidator.js
+++ middleware/validators/blogValidator.js
@@ -1,75 +1,97 @@
-const joi = require('joi');
-const HttpStatusCode = require('http-status-codes');
+const joi = require("joi");
+const HttpStatusCode = require("http-status-codes");
 
 class BlogValidator {
-    constructor() { }
+  constructor() {}
 
-    static async insert(req, res, next) {
-        try {
-            await joi.object({
-                BlogTitle: joi.string().required(),
-                BlogDescription: joi.string().required(),
-                BlogContent: joi.string().required(),
-                BlogState: joi.boolean(),
-                BlogMenuID: joi.number().required()
-            }).validateAsync(req.body);
-            next();
-        } catch (error) {
-            res.status(HttpStatusCode.EXPECTATION_FAILED).send('Must have correct data entry.');
-        }
+  static async insert(req, res, next) {
+    try {
+      await joi
+        .object({
+          BlogTitle: joi.string().required(),
+          BlogDescription: joi.string().required(),
+          BlogContent: joi.string().required(),
+          BlogState: joi.boolean(),
+          BlogMenuID: joi.number().required(),
+        })
+        .validateAsync(req.body);
+      next();
+    } catch (error) {
+      res
+        .status(HttpStatusCode.EXPECTATION_FAILED)
+        .send("Must have correct data entry.");
     }
+  }
 
-    static async update(req, res, next) {
-        try {
-            await joi.object({
-                BlogID: joi.number().min(1).required(),
-                BlogTitle: joi.string().required(),
-                BlogDescription: joi.string().required(),
-                BlogContent: joi.string().required(),
-                BlogState: joi.boolean(),
-                BlogMenuID: joi.number().required()
-            }).validateAsync(req.body);
-            next();
-        } catch (error) {
-            res.status(HttpStatusCode.EXPECTATION_FAILED).send('Must have correct data entry.');
-        }
+  static async update(req, res, next) {
+    try {
+      await joi
+        .object({
+          BlogID: joi.number().min(1).required(),
+          BlogTitle: joi.string().required(),
+          BlogDescription: joi.string().required(),
+          BlogContent: joi.string().required(),
+          BlogState: joi.boolean(),
+          BlogMenuID: joi.number().required(),
+        })
+        .validateAsync(req.body);
+      next();
+    } catch (error) {
+      res
+        .status(HttpStatusCode.EXPECTATION_FAILED)
+        .send("Must have correct data entry.");
     }
+  }
 
-    static async delete(req, res, next) {
-        try {
-            await joi.object({
-                BlogID: joi.number().min(1).required(),
-            }).validateAsync(req.body);
-            next();
-        } catch (error) {
-            res.status(HttpStatusCode.EXPECTATION_FAILED).send('Must have correct data entry.');
-        }
+  static async delete(req, res, next) {
+    try {
+      await joi
+        .object({
+          BlogID: joi.number().min(1).required(),
+        })
+        .validateAsync(req.body);
+      next();
+    } catch (error) {
+      res
+        .status(HttpStatusCode.EXPECTATION_FAILED)
+        .send("Must have correct data entry.");
     }
+  }
 
-    static async find(req, res, next) {
-        try {
-            await joi.object({
-                BlogID: joi.number().min(1).required(),
-            }).validateAsync({ BlogID: parseInt(req.params.BlogID) });
-            next();
-        } catch (error) {
-            res.status(HttpStatusCode.EXPECTATION_FAILED).send('Must have correct data entry.');
-        }
+  static async find(req, res, next) {
+    try {
+      await joi
+        .object({
+          BlogID: joi.number().min(1).required(),
+        })
+        .validateAsync({ BlogID: parseInt(req.params.BlogID) });
+      next();
+    } catch (error) {
+      res
+        .status(HttpStatusCode.EXPECTATION_FAILED)
+        .send("Must have correct data entry.");
     }
+  }
 
-    static async list(req, res, next) {
-        try {
-            await joi.object({
-                limit: joi.number(),
-                offset: joi.number(),
-                BlogState: joi.boolean(),
-                UserID: joi.number()
-            }).with('offset', 'limit').validateAsync(req.query);
-            next();
-        } catch (error) {
-            res.status(HttpStatusCode.EXPECTATION_FAILED).send('Must have correct data entry.');
-        }
+  static async list(req, res, next) {
+    try {
+      await joi
+        .object({
+          limit: joi.number(),
+          offset: joi.number(),
+          BlogState: joi.boolean(),
+          UserID: joi.number(),
+          BlogMenuID: joi.number(),
+        })
+        .with("offset", "limit")
+        .validateAsync(req.query);
+      next();
+    } catch (error) {
+      res
+        .status(HttpStatusCode.EXPECTATION_FAILED)
+        .send("Must have correct data entry.");
     }
+  }
 }
 
 module.exports = BlogValidator;
