ccb8534046077d6f579df05c353a3c5d93280887
ismet kizgin <info@ismetkizgin.com>
2021-01-18 01:17:28 +0300

    fix: fix for user find router

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,68 +1,138 @@
-const router = require('express')();
-const TransactionsFactory = require('../database/transactionFactory');
-const { validators, verifyToken, authorization } = require('../middleware');
-const userTransactions = TransactionsFactory.creating('userTransactions');
+const router = require("express")();
+const TransactionsFactory = require("../database/transactionFactory");
+const { validators, verifyToken, authorization } = require("../middleware");
+const userTransactions = TransactionsFactory.creating("userTransactions");
 const userValidator = validators.userValidator;
 const tokenControl = verifyToken.tokenControl;
 const authControl = authorization.authControl;
 const userInsertAuthControl = authorization.userInsertAuthControl;
-const HttpStatusCode = require('http-status-codes');
-const moment = require('moment');
+const HttpStatusCode = require("http-status-codes");
+const moment = require("moment");
+let { routerAuthorization } = require("../utils");
+routerAuthorization = routerAuthorization["user"];
 
-router.get('/user', tokenControl, authControl, userValidator.list, async (req, res) => {
+router.get(
+  "/user",
+  tokenControl,
+  authControl,
+  userValidator.list,
+  async (req, res) => {
     try {
-        const result = await userTransactions.listAsync(req.query);
-        res.json(result);
+      const result = await userTransactions.listAsync(req.query);
+      res.json(result);
     } catch (error) {
-        res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
+      res
+        .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(error.message);
     }
-});
+  }
+);
 
-router.get('/user/:UserID', tokenControl, authControl, userValidator.find, async (req, res) => {
+router.get(
+  "/user/:UserID",
+  tokenControl,
+  userValidator.find,
+  async (req, res) => {
     try {
-        const result = await userTransactions.findAsync({ UserID: req.params.UserID, UserTypeName: req.decode.UserTypeName });
-        res.json(result);
+      if (
+        routerAuthorization[req.method].Individual_Authorize.indexOf(
+          req.decode.UserTypeName
+        ) === -1
+      )
+        req.params.UserID = req.decode.UserID;
+      const result = await userTransactions.findAsync({
+        UserID: req.params.UserID,
+        UserTypeName: req.decode.UserTypeName,
+      });
+      res.json(result);
     } catch (error) {
-        res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
+      res
+        .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(error.message);
     }
-});
+  }
+);
 
-router.delete('/user', tokenControl, authControl, userValidator.delete, async (req, res) => {
+router.delete(
+  "/user",
+  tokenControl,
+  authControl,
+  userValidator.delete,
+  async (req, res) => {
     try {
-        await userTransactions.statusFindAsync({ UserID: req.body.UserID, UserTypeName: req.decode.UserTypeName });
-        const result = await userTransactions.deleteAsync(req.body.UserID);
-        res.json(result);
+      await userTransactions.statusFindAsync({
+        UserID: req.body.UserID,
+        UserTypeName: req.decode.UserTypeName,
+      });
+      const result = await userTransactions.deleteAsync(req.body.UserID);
+      res.json(result);
     } catch (error) {
-        if (error.status == 404)
-            res.status(HttpStatusCode.UNAUTHORIZED).send('User is not registered in the system or unauthorized operation.');
-        else
-            res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
+      if (error.status == 404)
+        res
+          .status(HttpStatusCode.UNAUTHORIZED)
+          .send(
+            "User is not registered in the system or unauthorized operation."
+          );
+      else
+        res
+          .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+          .send(error.message);
     }
-});
+  }
+);
 
-router.put('/user', tokenControl, authControl, userValidator.update, userInsertAuthControl, async (req, res) => {
+router.put(
+  "/user",
+  tokenControl,
+  authControl,
+  userValidator.update,
+  userInsertAuthControl,
+  async (req, res) => {
     try {
-        await userTransactions.statusFindAsync({ UserID: req.body.UserID, UserTypeName: req.decode.UserTypeName });
-        req.body.UserDateOfBirth = moment(new Date(req.body.UserDateOfBirth)).format('YYYY/MM/DD');
-        const result = await userTransactions.updateAsync(req.body);
-        res.json(result);
+      await userTransactions.statusFindAsync({
+        UserID: req.body.UserID,
+        UserTypeName: req.decode.UserTypeName,
+      });
+      req.body.UserDateOfBirth = moment(
+        new Date(req.body.UserDateOfBirth)
+      ).format("YYYY/MM/DD");
+      const result = await userTransactions.updateAsync(req.body);
+      res.json(result);
     } catch (error) {
-        if (error.status == 404)
-            res.status(HttpStatusCode.UNAUTHORIZED).send('User is not registered in the system or unauthorized operation.');
-        else
-            res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
+      if (error.status == 404)
+        res
+          .status(HttpStatusCode.UNAUTHORIZED)
+          .send(
+            "User is not registered in the system or unauthorized operation."
+          );
+      else
+        res
+          .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+          .send(error.message);
     }
-});
+  }
+);
 
-router.post('/user', tokenControl, authControl, userValidator.insert, userInsertAuthControl, async (req, res) => {
+router.post(
+  "/user",
+  tokenControl,
+  authControl,
+  userValidator.insert,
+  userInsertAuthControl,
+  async (req, res) => {
     try {
-        req.body.UserDateOfBirth = moment(new Date(req.body.UserDateOfBirth)).format('YYYY/MM/DD');
-        const result = await userTransactions.insertAsync(req.body);
-        res.json(result);
+      req.body.UserDateOfBirth = moment(
+        new Date(req.body.UserDateOfBirth)
+      ).format("YYYY/MM/DD");
+      const result = await userTransactions.insertAsync(req.body);
+      res.json(result);
     } catch (error) {
-        console.log(error);
-        res.status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR).send(error.message);
+      console.log(error);
+      res
+        .status(error.status || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .send(error.message);
     }
-});
+  }
+);
 
 module.exports = router;
--- utils/routerAuthorization.js
+++ utils/routerAuthorization.js
@@ -1,88 +1,89 @@
-const Roles = require('../models/roles');
+const Roles = require("../models/roles");
 
 module.exports = {
-    user: {
-        GET: {
-            Authorize: [Roles.Root, Roles.Administrator]
-        },
-        DELETE: {
-            Authorize: [Roles.Root, Roles.Administrator]
-        },
-        PUT: {
-            Authorize: [Roles.Root, Roles.Administrator]
-        },
-        POST: {
-            Authorize: [Roles.Root, Roles.Administrator]
-        }
+  user: {
+    GET: {
+      Authorize: [Roles.Root, Roles.Administrator],
+      Individual_Authorize: [Roles.Developer, Roles.Editor, Roles.User],
     },
-    component_menu: {
-        GET: {
-            Authorize: [Roles.Root, Roles.Administrator]
-        },
-        POST: {
-            Authorize: [Roles.Root, Roles.Administrator]
-        },
-        PUT: {
-            Authorize: [Roles.Root, Roles.Administrator]
-        },
-        DELETE: {
-            Authorize: [Roles.Root, Roles.Administrator]
-        }
+    DELETE: {
+      Authorize: [Roles.Root, Roles.Administrator],
     },
-    blog_menu: {
-        GET: {
-            Authorize: [Roles.Root, Roles.Administrator]
-        },
-        POST: {
-            Authorize: [Roles.Root, Roles.Administrator]
-        },
-        PUT: {
-            Authorize: [Roles.Root, Roles.Administrator]
-        },
-        DELETE: {
-            Authorize: [Roles.Root, Roles.Administrator]
-        }
+    PUT: {
+      Authorize: [Roles.Root, Roles.Administrator],
     },
-    component: {
-        POST: {
-            Authorize: [Roles.Root, Roles.Administrator, Roles.Developer]
-        },
-        PUT: {
-            Authorize: [Roles.Root, Roles.Administrator, Roles.Developer],
-            Individual_Authorize: [Roles.Developer]
-        },
-        DELETE: {
-            Authorize: [Roles.Root, Roles.Administrator, Roles.Developer],
-            Individual_Authorize: [Roles.Developer]
-        },
-        GET: {
-            Authorize: [Roles.Root, Roles.Administrator, Roles.Developer],
-            Individual_Authorize: [Roles.Developer]
-        },
+    POST: {
+      Authorize: [Roles.Root, Roles.Administrator],
     },
-    blog: {
-        POST: {
-            Authorize: [Roles.Root, Roles.Administrator, Roles.Editor]
-        },
-        PUT: {
-            Authorize: [Roles.Root, Roles.Administrator, Roles.Editor],
-            Individual_Authorize: [Roles.Editor]
-        },
-        DELETE: {
-            Authorize: [Roles.Root, Roles.Administrator, Roles.Developer],
-            Individual_Authorize: [Roles.Developer]
-        },
-        GET: {
-            Authorize: [Roles.Root, Roles.Administrator, Roles.Developer],
-            Individual_Authorize: [Roles.Developer]
-        }
+  },
+  component_menu: {
+    GET: {
+      Authorize: [Roles.Root, Roles.Administrator],
     },
-    project: {
-        GET: {
-            Public_Authorize: [Roles.Root, Roles.Administrator],
-        },
-        DELETE: {
-            Public_Authorize: [Roles.Root, Roles.Administrator],
-        }
-    }
-}
+    POST: {
+      Authorize: [Roles.Root, Roles.Administrator],
+    },
+    PUT: {
+      Authorize: [Roles.Root, Roles.Administrator],
+    },
+    DELETE: {
+      Authorize: [Roles.Root, Roles.Administrator],
+    },
+  },
+  blog_menu: {
+    GET: {
+      Authorize: [Roles.Root, Roles.Administrator],
+    },
+    POST: {
+      Authorize: [Roles.Root, Roles.Administrator],
+    },
+    PUT: {
+      Authorize: [Roles.Root, Roles.Administrator],
+    },
+    DELETE: {
+      Authorize: [Roles.Root, Roles.Administrator],
+    },
+  },
+  component: {
+    POST: {
+      Authorize: [Roles.Root, Roles.Administrator, Roles.Developer],
+    },
+    PUT: {
+      Authorize: [Roles.Root, Roles.Administrator, Roles.Developer],
+      Individual_Authorize: [Roles.Developer],
+    },
+    DELETE: {
+      Authorize: [Roles.Root, Roles.Administrator, Roles.Developer],
+      Individual_Authorize: [Roles.Developer],
+    },
+    GET: {
+      Authorize: [Roles.Root, Roles.Administrator, Roles.Developer],
+      Individual_Authorize: [Roles.Developer],
+    },
+  },
+  blog: {
+    POST: {
+      Authorize: [Roles.Root, Roles.Administrator, Roles.Editor],
+    },
+    PUT: {
+      Authorize: [Roles.Root, Roles.Administrator, Roles.Editor],
+      Individual_Authorize: [Roles.Editor],
+    },
+    DELETE: {
+      Authorize: [Roles.Root, Roles.Administrator, Roles.Developer],
+      Individual_Authorize: [Roles.Developer],
+    },
+    GET: {
+      Authorize: [Roles.Root, Roles.Administrator, Roles.Developer],
+      Individual_Authorize: [Roles.Developer],
+    },
+  },
+  project: {
+    GET: {
+      Public_Authorize: [Roles.Root, Roles.Administrator],
+    },
+    DELETE: {
+      Public_Authorize: [Roles.Root, Roles.Administrator],
+    },
+  },
+};
